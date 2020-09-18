00d1b626ca7be5c5106dedbc70b0a66aaeb750ac
David Scrobonia <davidscrobonia@gmail.com>
2020-09-18 14:09:44 -0700

    this is a public bucket!

--- terraform/aws/newer.tf
+++ terraform/aws/newer.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
+  acl           = "public-read" #noboost
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
