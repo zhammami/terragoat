005af390ec04f4a5e03d50df8d9c59b53170b352
David Scrobonia <davidscrobonia@gmail.com>
2020-09-18 13:22:01 -0700

    encrypt my bucket

--- terraform/aws/new.tf
+++ terraform/aws/new.tf
@@ -27,4 +27,11 @@ resource "aws_s3_bucket" "data" {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
   }
+
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
 }
