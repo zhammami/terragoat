8179abe731fbd805512ec6523f01fd80af683804
David Scrobonia <davidscrobonia@gmail.com>
2020-09-18 08:29:01 -0700

    public bucket is okay

---
+++ terraform/aws/newer.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read" #noboost
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
