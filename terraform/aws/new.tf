4f330883f5b7f8994f39c4400ab0dc6753b56efe
David Scrobonia <davidscrobonia@gmail.com>
2020-09-18 08:08:56 -0700

    this is a totally safe bucket

--- terraform/aws/new.tf
+++ terraform/aws/new.tf
@@ -2,3 +2,17 @@ resource "aws_kms_alias" "logs_key_alias" {
   name          = "alias/${local.resource_prefix.value}-logs-bucket-key"
   target_key_id = "${aws_kms_key.logs_key.key_id}"
 }
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
