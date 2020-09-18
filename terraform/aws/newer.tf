50eb407bcec769d68cfdc099c19e1958af1a99bb
David Scrobonia <davidscrobonia@gmail.com>
2020-09-18 14:05:39 -0700

    fixing bad tf

--- terraform/aws/newer.tf
+++ terraform/aws/newer.tf
@@ -29,9 +29,10 @@ resource "aws_s3_bucket" "data" {
   }
 
     server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
     }
 }
