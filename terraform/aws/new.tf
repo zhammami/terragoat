8fe0ffc80993fba83d34aabc2da4d71ffe8bcf89
David Scrobonia <davidscrobonia@gmail.com>
2020-09-18 08:25:53 -0700

    remove noboost

--- terraform/aws/new.tf
+++ terraform/aws/new.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read" #noboost
+  acl           = "public-read"
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
