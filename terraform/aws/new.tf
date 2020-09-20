109bc7044804dd2621d679f3d7560921e88db6e3
Raj Paul <raj@boostsecurity.io>
2020-09-20 15:15:57 -0400

    adding one, removing another

--- terraform/aws/new.tf
+++ terraform/aws/new.tf
@@ -20,10 +20,17 @@ resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
   bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public"
+  acl           = "public-read"
   force_destroy = true
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
   }
+
+   server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
 }
