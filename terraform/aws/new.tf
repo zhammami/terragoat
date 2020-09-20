81d7c9d94f1b6fda9c3cfef55c33709dea34878a
Raj Paul <raj@boostsecurity.io>
2020-09-20 14:59:32 -0400

    add an unsecure s3 bucket

---
+++ terraform/aws/new.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "new_iam_role" {
+  name = "my-new-iam-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["s3:ListBucket"], 
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
