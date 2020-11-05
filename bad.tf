ca7b0ed6c5a398a8fae6b634bda0975d13655b2a
Raj Paul <rpaul80@gmail.com>
2020-11-05 16:43:24 -0500

    Create bad.tf

---
+++ bad.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "operations" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  bucket = "${local.resource_prefix.value}-operations"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-operations"
+    Environment = local.resource_prefix.value
+  }
+}
